fb6c88322695a5ebd85f73c36b36857f112c73d0
Bruce Cowan <bruce@bcowan.me.uk>
2015-10-20 17:06:23 +0800

    Support all algorithms
    Next up - putting data into csv files

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import time
 
 import cv2
@@ -5,10 +7,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 class Benchmark:
-	def __init__(self):
-		self.detector = cv2.ORB()
-		self.descriptor = cv2.ORB()
+	def __init__(self, detector, descriptor):
+		if detector not in ["FAST", "STAR", "ORB", "BRISK", "MSER",
+                                    "GFTT", "HARRIS", "Dense", "SimpleBlob"]: # removed SIFT and SURF
+			raise ValueError("Unsupported detector")
+		if descriptor not in ["BRIEF", "BRISK", "ORB", "FREAK"]: # removed SIFT and SURF
+			raise ValueError("Unsupported descriptor")
 
+		self.detector = cv2.FeatureDetector_create(detector)
+		self.descriptor = cv2.DescriptorExtractor_create(descriptor)
 		self.times = []
 
 	def get_descriptors(self, frame):
@@ -17,20 +24,28 @@ class Benchmark:
 		(keypoints, descriptors) = self.descriptor.compute(frame, keypoints)
 		end = time.clock()
 
-		self.times.append(end - start)
+		self.times.append((end - start) * 1000) # convert to milliseconds
 
 		return (keypoints, descriptors)
 
 if __name__ == "__main__":
-	bench = Benchmark()
+	detector = 'SimpleBlob'
+	descriptor = 'FREAK'
+
+	bench = Benchmark(detector, descriptor)
 	cap = cv2.VideoCapture('test.mov')
 
 	while True:
 		(ret, frame) = cap.read()
-		bench.get_descriptors(frame)
 		if ret == False:
 			break
 
+		grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to greyscale
+		bench.get_descriptors(grey)
+
 	times = bench.times
 	plt.boxplot(times, vert=False)
+	plt.xlabel('Time / ms')
+	plt.ylabel('')
+	plt.title('{}/{}'.format(detector, descriptor))
 	plt.show()
