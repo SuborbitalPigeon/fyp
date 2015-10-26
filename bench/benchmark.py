c7765095bff6d4251703494ffecf9ff545bd892e
Bruce Cowan <bruce@bcowan.me.uk>
2015-10-26 23:40:34 +0800

    A few fixes across the code
    Most of this is indentation fixing

--- bench/benchmark.py
+++ bench/benchmark.py
@@ -1,110 +1,105 @@
 #!/usr/bin/env python
 
-import csv
 from __future__ import division
+
+import csv
 import time
 
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-from fileutils import get_images_from_dir
+from fileutils import get_images_from_dirs
 
 # removed SIFT and SURF from both lists
 detectors = ['FAST', 'STAR', 'ORB', 'BRISK', 'MSER', 'GFTT', 'HARRIS', 'Dense', 'SimpleBlob']
 descriptors = ['BRIEF', 'BRISK', 'ORB', 'FREAK']
 
 class Benchmark:
-	def __init__(self, detector, descriptor):
-		if detector not in detectors:
-			raise ValueError("Unsupported detector")
-		if descriptor not in descriptors: 
-			raise ValueError("Unsupported descriptor")
+    def __init__(self, detector, descriptor):
+        if detector not in detectors:
+            raise ValueError("Unsupported detector")
+        if descriptor not in descriptors: 
+            raise ValueError("Unsupported descriptor")
 
-		self.detector = cv2.FeatureDetector_create(detector)
-		self.descriptor = cv2.DescriptorExtractor_create(descriptor)
-		self.times = []
-		self.nkps = []
+        self.detector = cv2.FeatureDetector_create(detector)
+        self.descriptor = cv2.DescriptorExtractor_create(descriptor)
+        self.times = []
+        self.nkps = []
 
-	def get_descriptors(self, frame):
-		start = time.clock()
-		keypoints = self.detector.detect(frame)
-		(keypoints, descriptors) = self.descriptor.compute(frame, keypoints)
-		end = time.clock()
+    def get_descriptors(self, frame):
+        start = time.clock()
+        keypoints = self.detector.detect(frame)
+        (keypoints, descriptors) = self.descriptor.compute(frame, keypoints)
+        end = time.clock()
 
-		self.times.append(1 / (end - start))
-		self.nkps.append(len(keypoints))
+        self.times.append(1 / (end - start))
+        self.nkps.append(len(keypoints))
 
-		return (keypoints, descriptors)
+        return (keypoints, descriptors)
 
-	def run_tests(self, filenames):
-     		for file in filenames:
-			image = cv2.imread(file, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-			(kps, descriptors) = self.get_descriptors(image)
-		return (self.times, self.nkps)
+    def run_tests(self, filenames):
+        for file in filenames:
+            image = cv2.imread(file, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+            (kps, descriptors) = self.get_descriptors(image)
+        return (self.times, self.nkps)
 
 def get_mean_stdev(data):
-	mean = np.mean(data)
-	stdev = np.std(data)
-	return (mean, stdev)
+    mean = np.mean(data)
+    stdev = np.std(data)
+    return (mean, stdev)
 
 if __name__ == '__main__':
-	times = {}
-	nkps = {} # number of keypoints
+    times = {}
+    nkps = {} # number of keypoints
 
-	count = 0
-	for detector in detectors:
-		for descriptor in descriptors:
-			count += 1
-			name = "{}/{}".format(detector, descriptor)
-			print("Running test {}/{}: {}".format(count, len(detectors) * len(descriptors), name))
+    count = 0
+    for detector in detectors:
+        for descriptor in descriptors:
+            count += 1
+            name = "{}/{}".format(detector, descriptor)
+            print("Running test {}/{}: {}".format(count, len(detectors) * len(descriptors), name))
 
-			bench = Benchmark(detector, descriptor)
-   			files = get_images_from_dir('bark')
-      			files += get_images_from_dir('bikes')
-   			files += get_images_from_dir('boat')
-			files += get_images_from_dir('graf')
-			files += get_images_from_dir('leuven')
-			files += get_images_from_dir('trees')
-			files += get_images_from_dir('ubc')
-			files += get_images_from_dir('wall')
+            bench = Benchmark(detector, descriptor)
+            dirs = ['bark', 'bikes', 'boat', 'graf', 'leuven', 'trees', 'ubc', 'wall']
+            files = get_images_from_dirs(dirs)
 
-			times[name], nkps[name] = bench.run_tests(files)
+            times[name], nkps[name] = bench.run_tests(files)
 
-			# FPS
-			(mean, stdev) = get_mean_stdev(times[name])
-			print("FPS - mean: {:.2f} Hz, stdev: {:.2f} Hz".format(mean, stdev))
+            # FPS
+            (mean, stdev) = get_mean_stdev(times[name])
+            print("FPS - mean: {:.2f} Hz, stdev: {:.2f} Hz".format(mean, stdev))
 
-			# Keypoints
-			(mean, stdev) = get_mean_stdev(nkps[name])
-			print("Keypoints - mean: {:.2f}, stdev: {:.2f}".format(mean, stdev))
+            # Keypoints
+            (mean, stdev) = get_mean_stdev(nkps[name])
+            print("Keypoints - mean: {:.2f}, stdev: {:.2f}".format(mean, stdev))
 
-	# FPS plot
-	plt.boxplot(times.values())
-	plt.title("Frame rate")
-	# replace this with "labels" keyword parameter if possible
-	plt.xticks(range(1, len(times.keys()) + 1), times.keys(), rotation=45)
-	plt.ylabel("Frame rate / FPS")
-	plt.show(block=False)
+    # FPS plot
+    plt.boxplot(times.values())
+    plt.title("Frame rate")
+    # replace this with "labels" keyword parameter if possible
+    plt.xticks(range(1, len(times.keys()) + 1), times.keys(), rotation=45)
+    plt.ylabel("Frame rate / FPS")
+    plt.show(block=False)
 
-	# Number of keypoints plot
-	plt.figure()
-	plt.boxplot(nkps.values())
-	plt.title("Keypoints")
-	# replace this with "labels" keyword parameter if possible
-	plt.xticks(range(1, len(times.keys()) + 1), times.keys(), rotation=45)
-	plt.ylabel("Keypoints")
-	plt.yscale('log')
-	plt.show()
+    # Number of keypoints plot
+    plt.figure()
+    plt.boxplot(nkps.values(), labels=times.keys())
+    plt.title("Keypoints")
+    # replace this with "labels" keyword parameter if possible
+    plt.xticks(range(1, len(times.keys()) + 1), times.keys(), rotation=45)
+    plt.ylabel("Keypoints")
+    plt.yscale('log')
+    plt.show()
 
-	# FPS CSV
-	with open('fps.csv', 'wb') as f:
-		writer = csv.writer(f)
-		writer.writerow(times.keys())
-		writer.writerows(zip(*times.values()))
+    # FPS CSV
+    with open('fps.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(times.keys())
+        writer.writerows(zip(*times.values()))
 
-	# Number of keypoints CSV
-	with open('nkps.csv', 'wb') as f:
-		writer = csv.writer(f)
-		writer.writerow(nkps.keys())
-		writer.writerows(zip(*nkps.values()))
+    # Number of keypoints CSV
+    with open('nkps.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(nkps.keys())
+        writer.writerows(zip(*nkps.values()))
--- bench/fileutils.py
+++ bench/fileutils.py
@@ -1,10 +1,18 @@
 import os
 
 def get_images_from_dir(dir):
-	files = []
+    files = []
 
-        for file in os.listdir(dir):
-        	if file.endswith('ppm'):
-	                files.append(os.path.join(dir, file))
+    for file in os.listdir(dir):
+        if file.endswith('ppm'):
+            files.append(os.path.join(dir, file))
 
-        return files
+    return files
+
+def get_images_from_dirs(dirs):
+    files = []
+
+    for dir in dirs:
+        files += get_images_from_dir(dir)
+
+    return files
