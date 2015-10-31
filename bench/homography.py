eebad67cd3d292f478d3740b087209e7aeb937df
Bruce Cowan <bruce@bcowan.me.uk>
2015-10-31 12:41:03 +0800

    Test homography matrix transformation

--- bench/homography.py
+++ bench/homography.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 def file_to_matrix(filename):
@@ -8,5 +9,18 @@ def file_to_matrix(filename):
     return np.array(data[:3], dtype=float) # strip the last newline
 
 if __name__ == '__main__':
+    cv2.namedWindow("img")
+    cv2.namedWindow("img2")
+    cv2.namedWindow("img2h")
+
+    img = cv2.imread('bark/img1.ppm')
+    cv2.imshow("img", img)
+
+    img2 = cv2.imread('bark/img2.ppm')
+    cv2.imshow("img2", img2)
+
     mat = file_to_matrix('bark/H1to2p')
-    print(mat)
+    img2h = cv2.warpPerspective(img, mat, (800, 600))
+    cv2.imshow("img2h", img2h)
+    
+    cv2.waitKey(0)
