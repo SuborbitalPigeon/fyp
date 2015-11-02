b075172fe64867d876ac3badd08112cdd2860462
Bruce Cowan <bruce@bcowan.me.uk>
2015-11-02 23:36:46 +0800

    Various improvements
    Only consider keypoints in common area
    Use 'boat' images, these have simpler homography matrices,
    and better results

--- bench/scalerotationinvariance.py
+++ bench/scalerotationinvariance.py
@@ -13,9 +13,6 @@ from benchmark import Benchmark
 
 THRESHOLD = 10
 
-#TODO look at https://github.com/Itseez/opencv/blob/master/modules/features2d/src/evaluation.cpp
-#TODO only consider keypoints within the transformed image plane
-
 class ScaleRotationInvariance(Benchmark):
     def __init__(self, dirs, fileexts):
         super(ScaleRotationInvariance, self).__init__(dirs, fileexts)
@@ -26,7 +23,19 @@ class ScaleRotationInvariance(Benchmark):
         p = np.vstack((p, 1))  # (x, y, 1)^T
         d = np.dot(h, p)       # h * p
         d = (d / d[2])[0:2] # Divide rows 1 and 2 by 3, return only these rows
-        return np.transpose(d)
+        return np.transpose(d)[0]
+
+    @staticmethod
+    def _point_in_image(pt, image):
+        # Checks the image at this point, and returns True if this is totally black
+        (x, y) = pt
+
+        try:
+            ret = np.any(image[x][y] == np.array([0, 0, 0]))
+        except IndexError: # if point is outside bounds of image
+            return False
+
+        return ret
 
     def run_test(self, detector, descriptor, label):
         pattern = re.compile('(\w+)/img(\d).(\w+)')
@@ -40,23 +49,32 @@ class ScaleRotationInvariance(Benchmark):
 
             if num is '1':
                 basepts = [point.pt for point in keypoints]
+                baseimg = image
             else:
+                pts = [] # current image's keypoints
                 tpts = [] # transformed base keypoints
 
+                size = image.shape[1::-1]
                 mat = np.loadtxt(os.path.join(dir, 'H1to{}p'.format(num)))
+                rimage = cv2.warpPerspective(baseimg, mat, size) # for filtering keypoints
 
                 # This image's keypoints
-                pts = [point.pt for point in keypoints]
+                for point in keypoints:
+                    if self._point_in_image(point.pt, rimage):
+                        pts.append(point.pt)
+
                 # The base image's keypoints, projection required
                 for point in basepts:
                     p = np.array([[point[0]], [point[1]]])
-                    tpts.append(self._transform_point(p, mat))
+                    tp = self._transform_point(p, mat)
+                    if self._point_in_image(tp, rimage):
+                        tpts.append(tp)
 
                 tpts = np.vstack(tpts)
                 pts = np.vstack(pts)
                 dist = distance.cdist(pts, tpts)
                 count = np.sum(np.any(dist < THRESHOLD, axis=1))
-                print("Image {} correspondence: {:.2f} %".format(num, (count / len(dist) * 100)))
+                print("Image {} correspondence: {}/{} {:.2f} %".format(num, count, len(dist), (count / len(dist) * 100)))
 
     def show_plots(self):
         raise NotImplementedError("Not implemented yet")
@@ -65,7 +83,7 @@ class ScaleRotationInvariance(Benchmark):
         raise NotImplementedError("Not implemented yet")
 
 if __name__ == '__main__':
-    dirs = ['bark']
-    bench = ScaleRotationInvariance(dirs, 'ppm')
+    dirs = ['boat']
+    bench = ScaleRotationInvariance(dirs, 'pgm')
 
     bench.run_tests()
