e5630ad34ec0571f815d565843d11d860f833ede
Bruce Cowan <bruce@bcowan.me.uk>
2016-01-18 16:15:26 +0800

    Change display transform to save transform
    More useful to have the transmformed image files than to display them

--- performance/savetransforms.py
+++ performance/savetransforms.py
@@ -7,23 +7,21 @@ import sys
 import cv2
 import numpy as np
 
-def show_image(dir, file, num):
-    basename = "img{}".format(num)
-    cv2.namedWindow(basename)
-    image = cv2.imread(os.path.join(dir, file))
-    cv2.imshow(basename, image)
-
+def write_image(dir, file, num):
     # Base image has no transformation required
     if num is '1':
         return
 
-    name = "img{}transform".format(num)
-    cv2.namedWindow(name)
+    wfile = os.path.join('transformed', dir, file)
+    wfile = wfile.replace('pgm', 'png')
+    wfile = wfile.replace('ppm', 'png')
+
     image = cv2.imread(os.path.join(dir, file.replace(num, "1"))) # read 'base' file
     m = np.loadtxt(os.path.join(dir, 'H1to{}p'.format(num)))
     size = image.shape[1::-1]
     warped = cv2.warpPerspective(image, m, size)
-    cv2.imshow(name, warped)
+
+    cv2.imwrite(wfile, warped)
 
 try:
     dir = sys.argv[1]
@@ -38,6 +36,6 @@ for file in os.listdir(dir):
         match = pattern.match(file)
         (num, ext) = match.groups()
         
-        show_image(dir, file, num)
+        write_image(dir, file, num)
 
 cv2.waitKey(0)
