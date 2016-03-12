d36175cd9ed0709aa678ab278acff216d4b878de
Bruce Cowan <bruce@bcowan.me.uk>
2016-03-12 12:43:30 +0800

    Get combined speed test to do same as speed test

--- performance/combinedspeedtest.py
+++ performance/combinedspeedtest.py
@@ -1,8 +1,13 @@
-from collections import OrderedDict
+#!/usr/bin/env python3
+
+import csv
+from os.path import join
 from time import process_time
 
 import cv2
 from cv2 import xfeatures2d
+from matplotlib import pyplot as plt
+import numpy as np
 
 from performancetest import PerformanceTest
 
@@ -16,31 +21,57 @@ class CombinedSpeedTest(PerformanceTest):
         self._algos.append(xfeatures2d.SIFT_create())
         self._algos.append(xfeatures2d.SURF_create())
 
-        self._times = OrderedDict()
-        self._nkps = OrderedDict()
+        self._times = {}
+        self._nkps = {}
 
         self._images = [cv2.imread(image) for image in self.files]
 
     def run_tests(self):
         for algo in self._algos:
-            print("Running test {}".format(algo))
+            label = str(algo).split()[0][1:] # Take only the algo name, and remove <
+            print("Running test {}".format(label))
+            times = []
+            nkps = []
+
             for image in self._images:
                 start = process_time()
                 kps, des = algo.detectAndCompute(image, None)
                 end = process_time()
-                usperkp = ((end - start)*1000*1000) / len(kps)
-                print("{} took {:.2f} us per keypoint ({} kps)".format(algo, usperkp, len(kps)))
+                times.append((end-start)*1000)
+                nkps.append(len(kps))
+
+            self._times[label] = np.array(times)
+            self._nkps[label] = np.array(nkps)
 
     def save_data(self):
-        pass
+        # FPS CSV
+        with open(join('results', 'combinedspeed.csv'), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(list(self._times.keys()))
+            writer.writerows(zip(*self._times.values()))
+
+        # Number of keypoints CSV
+        with open(join('results', 'combinednkps.csv'), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(list(self._nkps.keys()))
+            writer.writerows(zip(*self._nkps.values()))
 
     def show_plots(self):
-        pass
+        plt.figure()
+        labels = list(self._times.keys())
+        data = list(self._times.values())
+        plt.boxplot(data, labels=labels, showmeans=True)
+        plt.title("Combined speed test")
+        plt.xticks(rotation=45)
+        plt.xlabel("Algorithm")
+        plt.ylabel("Time taken / ms")
+        plt.draw()
+        plt.savefig(join("results", "combinedspeed.pdf"))
 
 if __name__ == '__main__':
     dirs = PerformanceTest.get_dirs_from_argv()
     test = CombinedSpeedTest(dirs=dirs, filexts=('pgm', 'ppm'))
 
     test.run_tests()
-    test.show_plots()
     test.save_data()
+    test.show_plots()
