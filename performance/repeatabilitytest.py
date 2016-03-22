a2bac0f996842288592668c56b5a84fdeb668e98
Bruce Cowan <bruce@bcowan.me.uk>
2016-03-22 14:18:36 +0800

    [Repeatability] Store points separately
    Removed graphing for now

--- performance/repeatabilitytest.py
+++ performance/repeatabilitytest.py
@@ -7,21 +7,20 @@ from os.path import join
 import re
 
 import cv2
-from matplotlib import pyplot as plt
-from matplotlib.ticker import FuncFormatter
 import numpy as np
 from scipy.spatial import distance
 
 from performancetest import PerformanceTest
 
-THRESHOLD = 10
+THRESHOLD = 2
 
 
 class RepeatabilityTest(PerformanceTest):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.data = OrderedDict()
+        self.common = OrderedDict()
+        self.baset = OrderedDict()
 
     def run_tests(self):
         count = 0
@@ -33,7 +32,8 @@ class RepeatabilityTest(PerformanceTest):
             self.run_test(detector, det)
 
     def run_test(self, label, detector):
-        repeatability = []
+        common = []
+        baset = []
         pattern = re.compile('(\w+)/img(\d).(\w+)')
 
         for file in self.files:
@@ -45,66 +45,51 @@ class RepeatabilityTest(PerformanceTest):
 
             if num is '1':
                 basepts = keypoints
-            else:
-                pts = [] # current image's keypoints
-                tpts = [] # transformed base keypoints
+                continue
 
-                mat = np.loadtxt(os.path.join(dir, 'H1to{}p'.format(num)))
-                mask = self.create_mask(image.shape, mat)
+            pts = [] # current image's keypoints
+            tpts = [] # transformed base keypoints
 
-                # This image's keypoints
-                for point in keypoints:
-                    if self.point_in_image(point, mask):
-                        pts.append(point.pt)
+            mat = np.loadtxt(os.path.join(dir, 'H1to{}p'.format(num)))
+            mask = self.create_mask(image.shape, mat)
 
-                # The base image's keypoints, projection required
-                for point in basepts:
-                    tp = self.transform_point(point , mat)
-                    if self.point_in_image(tp, mask):
-                        tpts.append(tp.pt)
+            # This image's keypoints
+            for point in keypoints:
+                if self.point_in_image(point, mask):
+                    pts.append(point.pt)
 
-                if len(pts) == 0:
-                    continue
+            # The base image's keypoints, projection required
+            for point in basepts:
+                tp = self.transform_point(point, mat)
+                if self.point_in_image(tp, mask):
+                    tpts.append(tp.pt)
 
-                tpts = np.vstack(tpts)
-                pts = np.vstack(pts)
+            tpts = np.vstack(tpts)
+            pts = np.vstack(pts)
+            if len(pts) > len(tpts):
                 dist = distance.cdist(pts, tpts).min(axis=1)
+            else:
+                dist = distance.cdist(pts, tpts).min(axis=0)
 
-                repeatability.append(np.sum(dist < THRESHOLD) / len(tpts))
+            common.append(len(tpts))
+            baset.append(np.sum(dist < THRESHOLD))
 
-            self.data[label] = repeatability
-
-    @staticmethod
-    def _percent_format(y, position):
-        s = str(y * 100)
-        return s + '%'
+        self.common[label] = common
+        self.baset[label] = baset
 
     def show_plots(self):
-        ytick = FuncFormatter(self._percent_format)
-        fnames = [f.split('/')[1] for f in self.files[1:]] # filenames
-
-        # TODO Linestyle changes?
-        colour = iter(plt.cm.jet(np.linspace(0,1,len(self.data))))
-
-        for key, val in self.data.items():
-            c = next(colour)
-            plt.plot(val, label=key, c=c)
-
-        plt.title("10-Repeatability")
-        plt.xticks(np.arange(len(fnames)), fnames)
-        plt.xlabel("Image")
-        plt.gca().yaxis.set_major_formatter(ytick)
-        plt.ylabel("10-Repeatability")
-        plt.ylim(0, 1) # 0 % -- 100 %
-        plt.legend(loc='best', framealpha=0.5)
-        plt.draw()
-        plt.savefig(join("results", "repeatability.pdf"))
+        pass
 
     def save_data(self):
-        with open(join('results', 'repeat.csv'), 'w') as f:
+        with open(join('results', 'repeat-common.csv'), 'w') as f:
             writer = csv.writer(f)
-            writer.writerow(list(self.data.keys()))
-            writer.writerows(zip(*self.data.values()))
+            writer.writerow(list(self.common.keys()))
+            writer.writerows(zip(*self.common.values()))
+
+        with open(join('results', 'repeat-baset.csv'), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(list(self.baset.keys()))
+            writer.writerows(zip(*self.baset.values()))
 
 if __name__ == '__main__':
     dirs = PerformanceTest.get_dirs_from_argv()
