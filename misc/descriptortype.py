a945c8d9daa1550ed4463b6d055224306ec80d0c
Bruce Cowan <bruce@bcowan.me.uk>
2016-03-22 15:37:24 +0800

    [Misc] Add descriptor type determining script

---
+++ misc/descriptortype.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import cv2
+from cv2 import xfeatures2d
+
+file = '../performance/boat/img1.pgm'
+img = cv2.imread(file)
+
+akaze = cv2.AKAZE_create()
+points = akaze.detect(img)
+
+descriptors = [akaze]
+descriptors.append(cv2.BRISK_create())
+descriptors.append(cv2.KAZE_create())
+descriptors.append(cv2.ORB_create())
+descriptors.append(xfeatures2d.BriefDescriptorExtractor_create())
+descriptors.append(xfeatures2d.DAISY_create())
+descriptors.append(xfeatures2d.FREAK_create())
+descriptors.append(xfeatures2d.LATCH_create())
+descriptors.append(xfeatures2d.LUCID_create(1 ,1))
+descriptors.append(xfeatures2d.SIFT_create())
+descriptors.append(xfeatures2d.SURF_create())
+
+for descriptor in descriptors:
+    kps, des = descriptor.compute(img, points)
+    print("Algorithm: {}, size: {}, type: {}".format(descriptor, des[0].size, des[0].dtype))
